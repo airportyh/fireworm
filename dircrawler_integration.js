c76acbad361f52021c2379e69b21477ced45f47e
airportyh <airportyh@gmail.com>
2013-08-10 00:13:51 -0400

    Added a manual test.

---
+++ dircrawler_integration.js
@@ -0,0 +1,13 @@
+var DirCrawler = require('./dircrawler')
+
+var c = new DirCrawler('./')
+c.add('*.js')
+c.add('*/*.js')
+
+c.crawl(function(){})
+
+c.on('change', function(filepath){
+  console.log(filepath, 'changed')
+})
+
+setInterval(function(){}, 1000)
